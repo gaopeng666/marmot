2f40ae51776590b73c500c8146d32849e08c5f12
gaopeng <450498893@qq.com>
2019-12-14 15:45:24 +0800

    代码提交

---
+++ src/main/java/testCI/TestCIBuild.java
@@ -0,0 +1,13 @@
+package testCI;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+
+public class TestCIBuild {
+    public static void main(String[] args) {
+        ArrayList<Object> list = Lists.newArrayList();
+        list.add(1);
+        list.add("");
+    }
+}
